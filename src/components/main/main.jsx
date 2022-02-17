6c888ac8b0d757b18f344e38102fcab8828556cd
Günther von Häfen <>
2022-02-17 12:27:03 +0100

    added an overlay on image for text contrast, smaller fontsize

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,11 +3,13 @@ const Main = () => {
         <main id='home'>
             <img className='mainBackground' src={process.env.PUBLIC_URL + '/img/DavutGoeren.jpg'} alt='Portraitfoto von Davon Gören' />
             <div className='mainContent'>
-                <p>Rechtsanwalt</p>
-                <h1>Davut Gören</h1>
-                <p><mark>Kompetente und effiziente<br />Rechtsberatung. Gemeinsam finden wir eine Lösung.</mark></p>
-                <div className='mainContent__link'>
-                    <a href='#references'>Mehr erfahren</a>
+                <div className='mainContent__text'>
+                    <p>Rechtsanwalt</p>
+                    <h1>Davut Gören</h1>
+                    <p><mark>Kompetente und effiziente<br />Rechtsberatung. Gemeinsam finden wir eine Lösung.</mark></p>
+                    <div className='mainContent__link'>
+                        <a href='#references'>Mehr erfahren</a>
+                    </div>
                 </div>
             </div>
         </main>
